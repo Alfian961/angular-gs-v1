48fef52c8afadabe6d659dde4aef1e7fb6952ae3
Alfian961 <scriptionap@hotmail.sg>
2022-03-29 15:55:18 +0800

    2 files modified, 1 added

---
+++ src/app/ejavascript.js
@@ -0,0 +1,13 @@
+function Pat() {
+  // alert is an inubuilt function
+  const audio = new Audio();
+  audio.src = 'images/cat.mp3';
+  audio.play();
+}
+
+function Hiss() {
+  // alert is an inubuilt function
+  const audio = new Audio();
+  audio.src = 'images/hiss.mp3';
+  audio.play();
+}
